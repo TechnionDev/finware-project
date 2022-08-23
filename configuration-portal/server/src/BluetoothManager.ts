c770a68ec9b44498e15e23bc0400c5bc9ea75dc8
Saar Ofek <saar.ofek3@gmail.com>
2022-08-24 00:59:34 +0300

    rpi

--- configuration-portal/server/src/BluetoothManager.ts
+++ configuration-portal/server/src/BluetoothManager.ts
@@ -46,7 +46,7 @@ export default class BluetoothManager {
 
         console.log('Connection established!');
         console.log('Security: ', conn.smp.currentEncryptionLevel, conn.smp.isEncrypted);
-        console.log(JSON.stringify(conn, null, 4));
+        console.log(conn);
 
         const IOCapabilities = NodeBleHost.IOCapabilities;
         const AssociationModels = NodeBleHost.AssociationModels;
