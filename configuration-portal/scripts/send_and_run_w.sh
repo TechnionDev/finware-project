06c60d1295a85ccd8bc4b6e3a579a56527dba640
Saar Ofek <saar.ofek3@gmail.com>
2022-09-19 01:27:57 +0300

    changed send script

--- configuration-portal/scripts/send_and_run_w.sh
+++ configuration-portal/scripts/send_and_run_w.sh
@@ -1,5 +1,5 @@
 npm run build
-scp -o NoHostAuthenticationForLocalhost=yes -r build src-backend icst@raspberrypi.local:~/opt/finware-project/configuration-portal/
+scp -o NoHostAuthenticationForLocalhost=yes -r build src-backend icst@raspberrypi.local:/opt/finware-project/configuration-portal/
 
 ssh -o NoHostAuthenticationForLocalhost=yes icst@raspberrypi.local 'source ~/.bashrc && cd /opt/finware-project/configuration-portal && /home/icst/.nvm/versions/node/v18.7.0/bin/npx tsc'
 
