9bd4feea5a3419b33ff8dacdbc890c1aeb4bd378
Gur Telem <gtelem@apple.com>
2022-10-01 02:05:24 +0300

    Remove a TODO (for auth that is already done)

--- configuration-portal/src-backend/routes/api/settings.ts
+++ configuration-portal/src-backend/routes/api/settings.ts
@@ -4,7 +4,6 @@ import { getSettings, updateSettings } from "../../util-managers/SettingsManager
 export default function settingsRouterFactory(financeAccountsController) {
   const settingsRouter = express.Router();
 
-  // TODO: Add authentication for this router
   settingsRouter.get("/get", getSettings);
   settingsRouter.put("/update", (req, res) => updateSettings(req, res, financeAccountsController));
 
