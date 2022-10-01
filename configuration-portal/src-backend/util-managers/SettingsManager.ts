50f08bc42e1301a287062b4237599b588a48acb7
Gur Telem <gtelem@apple.com>
2022-10-01 22:57:21 +0300

    Fix bug with password changing to empty string when modifying other settings

--- configuration-portal/src-backend/util-managers/SettingsManager.ts
+++ configuration-portal/src-backend/util-managers/SettingsManager.ts
@@ -9,13 +9,15 @@ const HIDDEN_PASSWORD_VALUE = ''
 async function updateSettings(req, res, financeAccountsController) {
     console.log("Updating settings:", req.body);
     let oldSettings = await SettingsModel.findOneAndUpdate({}, {}, { upsert: true, new: true });
-    let settings = await SettingsModel.findOneAndUpdate({}, req.body, { upsert: true, new: true });
-    if (req.body?.month_cycle_start_day != oldSettings.month_cycle_start_day) {
-        financeAccountsController.updateFinancialData(true);
-    }
     if (req.body.password_hash == HIDDEN_PASSWORD_VALUE) {
         req.body.password_hash = undefined;
         delete req.body.password_hash;
+    } else {
+        console.log(`password change detected (set to "${req.body.password_hash}")`);
+    }
+    let settings = await SettingsModel.findOneAndUpdate({}, req.body, { upsert: true, new: true });
+    if (req.body?.month_cycle_start_day != oldSettings.month_cycle_start_day) {
+        financeAccountsController.updateFinancialData(true);
     }
     res.json(settings);
 }
